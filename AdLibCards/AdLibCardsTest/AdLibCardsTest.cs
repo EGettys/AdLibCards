8f2c8c3a753b0dd0895350910efff11080daaa2d
EGettys <elizabethanngettys@gmail.com>
2022-06-12 18:01:22 -0700

    Preliminary question card tests passed.

---
+++ AdLibCards/AdLibCardsTest/AdLibCardsTest.cs
@@ -0,0 +1,26 @@
+using AdLibCardsLibrary;
+namespace AdLibCardsTest
+{
+    [TestClass]
+    public class AdlibCardsTest
+    {
+        [TestMethod]
+        public void Question_Card_No_Blank()
+        {
+            Question_Card qcard = new Question_Card("What makes life worth living?");
+            Assert.AreEqual((byte) 0, qcard.number_blanks, "There should be 0 blanks.");
+        }
+        [TestMethod]
+        public void Question_Card_1_Blank()
+        {
+            Question_Card qcard = new Question_Card("__.It's a trap!");
+            Assert.AreEqual((byte)1, qcard.number_blanks, "There should be 1 blank.");
+        }
+        [TestMethod]
+        public void Question_Card_3_Blank()
+        {
+            Question_Card qcard = new Question_Card("_ + _ = _");
+            Assert.AreEqual((byte)3, qcard.number_blanks, "There should be 3 blanks.");
+        }
+    }
+}
